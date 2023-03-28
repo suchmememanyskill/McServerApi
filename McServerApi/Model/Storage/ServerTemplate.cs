ecdbe0512642f2671e2e21970ac2188f5b363954
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2023-03-28 23:25:48 +0200

    fug

--- McServerApi/Model/Storage/ServerTemplate.cs
+++ McServerApi/Model/Storage/ServerTemplate.cs
@@ -7,7 +7,7 @@ public class ServerTemplate
     public string Version { get; set; } = "1.0.0";
     public string JavaVersion { get; set; } = "latest";
     [JsonIgnore]
-    public bool UsesMaps => string.IsNullOrWhiteSpace(AbsoluteServerPath) && !AbsoluteServerPath.EndsWith(".sh");
+    public bool UsesMaps => string.IsNullOrWhiteSpace(AbsoluteServerPath) || (AbsoluteServerPath?.EndsWith(".sh") ?? false);
 
     [System.Text.Json.Serialization.JsonIgnore]
     public List<string> Mappings { get; set; } = new();
